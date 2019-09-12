81fc84b921af61d868e151baf10ebab2b7394230
Thomas Lee <thomaslee9474@gmail.com>
2019-09-12 07:59:34 +0000

    [added] loop

--- BUILD
+++ BUILD
@@ -2,3 +2,8 @@ cc_binary(
     name = "random",
     srcs = ["random.cpp"],
 )
+
+cc_binary(
+    name = "loops",
+    srcs = ["loops.cpp"],
+)
---
+++ loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv){
+
+  for(int i = 1; i <= 10; i++){
+    std::cout << i << std::endl;
+  }
+
+  return 0;
+  
+
+}
